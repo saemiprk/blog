ea3473f9c95f81cd9dc7be09a459dc6f395bf390
Saemi Park <saemiprk@gmail.com>
2024-02-11 21:00:55 +0900

    0211

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  // state의 장점은 데이터가 변경 되면 자동으로 재렌더링 됨. 
+  // state가 아닌 변수로 지정 시 새로고침으로 변경 됨.
+  // 제목 같은 경우는 수시로 바뀌는것이 아니기 때문으로 state로 관리 하지 않아도 됨.
+
+  let [title, setTitle] = useState(['Intro', 'Gallery', 'Infomation']);
+  let [date, setDate] = useState(['2월 17일 발행', '2월 18일 발행', '2월 19일 발행']);
+  let [num, setNum] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='black-nav'>
+        <div>Blog</div>
+      </div>
+      <div className='list'>
+          <h3>{title[0]} <span className='sugg' onClick={ () => setNum(num++)}>👍</span> {num} </h3>
+          <p>{date[0]}</p>
+          <hr />
+      </div>
+
+    <Modal></Modal>
+      {/* <div className='modal'>
+          <h2>제목</h2>
+          <p>날짜</p>
+          <p>상세내용</p>
+      </div> */}
     </div>
-  );
+  )
 }
 
 export default App;
